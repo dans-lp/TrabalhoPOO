b76a2b64d00f52abfc3936a6dcb370a95ccb16f6
aleAlmeiiida <89938609+aleAlmeiiida@users.noreply.github.com>
2021-11-24 21:28:26 -0300

    Update Peao.java

--- src/Model/Peao.java
+++ src/Model/Peao.java
@@ -1,25 +1,25 @@
-package Model;
+package model;
 
 public class Peao {
 	
 	private final Cor cor;
 	private final Jogador jogador;
 	private final int num;
-	private final byte poloInicial;
-	private final byte poloOposto;
-	private byte poloAtual;
-	private byte casa;
+	private final int poloInicial;
+	/*private final byte poloOposto;
+	private byte poloAtual;*/
+	private int casa;
 	
 	public Peao(Jogador dono, int numPeao){
 		cor = dono.corDoJogador();
 		jogador = dono;
 		num = numPeao;
 		poloInicial = dono.poloInicialDoJogdor();
-		poloAtual = casa = poloInicial;
+		/*poloAtual = casa = poloInicial;
 		if (poloInicial == 0) 
 			poloOposto = 73;
 		else 
-			poloOposto = 0;
+			poloOposto = 0;*/
     }
 	
 	public Cor corDoPeao() {
@@ -34,11 +34,11 @@ public class Peao {
 		return num;
 	}
 	
-	public byte poloInicialDoPeao() {
+	public int poloInicialDoPeao() {
 		return poloInicial;
 	}
 	
-	public byte poloOpostoDoPeao() {
+	/*public byte poloOpostoDoPeao() {
 		return poloOposto;
 	}
 	
@@ -52,13 +52,13 @@ public class Peao {
 		else 
 			poloAtual = 0;
 		return;
-	}
+	}*/
 	
-	public byte casaDoPeao() {
+	public int casaDoPeao() {
 		return casa;
 	}
 	
-	public void mudarCasaDoPeao(byte novaCasa) {
+	public void mudarCasaDoPeao(int novaCasa) {
 		casa = novaCasa;
 		return;
 	}
